d485b793406e7c508a77fdb648cb95ee9d052be5
William Hunt <rionhunt2@gmail.com>
2021-07-09 08:50:10 -0400

    Added a button and an event listener to roll dice. Added BootStrap to style the button. Modified the btn div for better spacing. Commented the program.

--- index.js
+++ index.js
@@ -1,74 +1,83 @@
-// JavaScript file to roll the dice and change the die images.
+/* Purpose:    JavaScript file to roll the dice and change the die images
+               when a button is clicked.
+   Programmer: William Hunt wrote the JavaScript.
+   Date:       9 July 2021
+*/
 
 var randomNumber1;
 var randomNumber2;
 var randomImage;
+const button = document.querySelector("button");
 
-// Function to roll the dice return a number between 1 and 6.
-function rollDice() {
-  randomNumber = Math.floor(Math.random() * 6) + 1;
-  return randomNumber;
-}
-// Roll and set first die.
-randomNumber1 = rollDice();
-if (randomNumber === 1) {
-  document
-    .getElementsByClassName("img1")[0]
-    .setAttribute("src", "images/dice1.png");
-} else if (randomNumber === 2) {
-  document
-    .getElementsByClassName("img1")[0]
-    .setAttribute("src", "images/dice2.png");
-} else if (randomNumber === 3) {
-  document
-    .getElementsByClassName("img1")[0]
-    .setAttribute("src", "images/dice3.png");
-} else if (randomNumber === 4) {
-  document
-    .getElementsByClassName("img1")[0]
-    .setAttribute("src", "images/dice4.png");
-} else if (randomNumber === 5) {
-  document
-    .getElementsByClassName("img1")[0]
-    .setAttribute("src", "images/dice5.png");
-} else if (randomNumber === 6) {
-  document
-    .getElementsByClassName("img6")[0]
-    .setAttribute("src", "images/dice6.png");
-}
+button.addEventListener("click", (e) => {
+  //console.log("button clicked");
 
-//Roll and set the second die.
-randomNumber2 = rollDice();
-if (randomNumber === 1) {
-  document
-    .getElementsByClassName("img2")[0]
-    .setAttribute("src", "images/dice1.png");
-} else if (randomNumber === 2) {
-  document
-    .getElementsByClassName("img2")[0]
-    .setAttribute("src", "images/dice2.png");
-} else if (randomNumber === 3) {
-  document
-    .getElementsByClassName("img2")[0]
-    .setAttribute("src", "images/dice3.png");
-} else if (randomNumber === 4) {
-  document
-    .getElementsByClassName("img2")[0]
-    .setAttribute("src", "images/dice4.png");
-} else if (randomNumber === 5) {
-  document
-    .getElementsByClassName("img2")[0]
-    .setAttribute("src", "images/dice5.png");
-} else if (randomNumber === 6) {
-  document
-    .getElementsByClassName("img2")[0]
-    .setAttribute("src", "images/dice6.png");
-}
-// Declare a winner.
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
-} else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-} else if (randomNumber2 === randomNumber1) {
-  document.querySelector("h1").innerHTML = "It's A Tie! Roll Again!";
-}
+  // Function to roll the dice return a number between 1 and 6.
+  function rollDice() {
+    randomNumber = Math.floor(Math.random() * 6) + 1;
+    return randomNumber;
+  }
+  // Roll and set first die.
+  randomNumber1 = rollDice();
+  if (randomNumber === 1) {
+    document
+      .getElementsByClassName("img1")[0]
+      .setAttribute("src", "images/dice1.png");
+  } else if (randomNumber === 2) {
+    document
+      .getElementsByClassName("img1")[0]
+      .setAttribute("src", "images/dice2.png");
+  } else if (randomNumber === 3) {
+    document
+      .getElementsByClassName("img1")[0]
+      .setAttribute("src", "images/dice3.png");
+  } else if (randomNumber === 4) {
+    document
+      .getElementsByClassName("img1")[0]
+      .setAttribute("src", "images/dice4.png");
+  } else if (randomNumber === 5) {
+    document
+      .getElementsByClassName("img1")[0]
+      .setAttribute("src", "images/dice5.png");
+  } else if (randomNumber === 6) {
+    document
+      .getElementsByClassName("img6")[0]
+      .setAttribute("src", "images/dice6.png");
+  }
+
+  //Roll and set the second die.
+  randomNumber2 = rollDice();
+  if (randomNumber === 1) {
+    document
+      .getElementsByClassName("img2")[0]
+      .setAttribute("src", "images/dice1.png");
+  } else if (randomNumber === 2) {
+    document
+      .getElementsByClassName("img2")[0]
+      .setAttribute("src", "images/dice2.png");
+  } else if (randomNumber === 3) {
+    document
+      .getElementsByClassName("img2")[0]
+      .setAttribute("src", "images/dice3.png");
+  } else if (randomNumber === 4) {
+    document
+      .getElementsByClassName("img2")[0]
+      .setAttribute("src", "images/dice4.png");
+  } else if (randomNumber === 5) {
+    document
+      .getElementsByClassName("img2")[0]
+      .setAttribute("src", "images/dice5.png");
+  } else if (randomNumber === 6) {
+    document
+      .getElementsByClassName("img2")[0]
+      .setAttribute("src", "images/dice6.png");
+  }
+  // Declare a winner.
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  } else if (randomNumber2 === randomNumber1) {
+    document.querySelector("h1").innerHTML = "It's A Tie! Roll Again!";
+  }
+});
